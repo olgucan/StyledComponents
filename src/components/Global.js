cf28d58a919959c6d5dac1166f7c5798c826bb76
olgucan <olgu.keser@gmail.com>
2022-05-14 16:19:35 +0300

    lamadev styled

---
+++ src/components/Global.js
@@ -0,0 +1,22 @@
+import {createGlobalStyle} from "styled-components";
+
+
+const GlobalStyles = createGlobalStyle `
+   * {
+       box-sizing:border-box;
+   } 
+   body {
+       background:${(({theme})=>theme.colors.body)};
+       color:hsl(192,100%,9%);
+       font-family:'Roboto', 'sans-serif';
+       font-size:1.15em;
+   }
+   p {
+       opacity:.6;
+       line-height:1.5;
+   }
+   img {
+       max-width:100%;
+   }
+`
+export default GlobalStyles
---
+++ src/pages/Create.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+
+const Create = () => {
+  return (
+    <div>
+      
+     <Typography
+      variant="h6"
+      component="h2"
+      color="textSecondary"
+      gutterBottom
+     >
+   create a new node
+     </Typography>
+    </div>
+  )
+}
+
+export default Create
